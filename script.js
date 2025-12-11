82b3c137f82c73be3071a079433328a9c4c4e6d7
kavindra-M <kavindram163@gmail.com>
2025-12-11 10:10:48 +0530

    Add initial portfolio files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scroll to Projects
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// Show console message
+console.log("Website Loaded Successfully!");
